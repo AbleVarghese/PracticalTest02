3fe7c4241b0b1c1b5753e294b56138c50f8d4017
Able Varghese <able.varghese12@gmail.com>
2015-03-25 10:14:53 -0400

    PracticalExamCommit

---
+++ PracticalExam02/AbilityForm.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace PracticalExam02
+{
+    partial class AbilityForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AbilityGroupBox = new System.Windows.Forms.GroupBox();
+            this.RollButton = new System.Windows.Forms.Button();
+            this.PhycheLabel = new System.Windows.Forms.Label();
+            this.IntuitionLabel = new System.Windows.Forms.Label();
+            this.ReasonLabel = new System.Windows.Forms.Label();
+            this.EnduranceLabel = new System.Windows.Forms.Label();
+            this.StrengthLabel = new System.Windows.Forms.Label();
+            this.AgilityLabel = new System.Windows.Forms.Label();
+            this.FightingLabel = new System.Windows.Forms.Label();
+            this.PhycheTextBox = new System.Windows.Forms.TextBox();
+            this.IntuitionTextBox = new System.Windows.Forms.TextBox();
+            this.ReasonTextBox = new System.Windows.Forms.TextBox();
+            this.StrengthTextBox = new System.Windows.Forms.TextBox();
+            this.EnduranceTextBox = new System.Windows.Forms.TextBox();
+            this.AgilityTextBox = new System.Windows.Forms.TextBox();
+            this.FightingTextBox = new System.Windows.Forms.TextBox();
+            this.AbilityGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AbilityGroupBox
+            // 
+            this.AbilityGroupBox.Controls.Add(this.PhycheLabel);
+            this.AbilityGroupBox.Controls.Add(this.IntuitionLabel);
+            this.AbilityGroupBox.Controls.Add(this.ReasonLabel);
+            this.AbilityGroupBox.Controls.Add(this.EnduranceLabel);
+            this.AbilityGroupBox.Controls.Add(this.StrengthLabel);
+            this.AbilityGroupBox.Controls.Add(this.AgilityLabel);
+            this.AbilityGroupBox.Controls.Add(this.FightingLabel);
+            this.AbilityGroupBox.Controls.Add(this.PhycheTextBox);
+            this.AbilityGroupBox.Controls.Add(this.IntuitionTextBox);
+            this.AbilityGroupBox.Controls.Add(this.ReasonTextBox);
+            this.AbilityGroupBox.Controls.Add(this.StrengthTextBox);
+            this.AbilityGroupBox.Controls.Add(this.EnduranceTextBox);
+            this.AbilityGroupBox.Controls.Add(this.AgilityTextBox);
+            this.AbilityGroupBox.Controls.Add(this.FightingTextBox);
+            this.AbilityGroupBox.Location = new System.Drawing.Point(12, 26);
+            this.AbilityGroupBox.Name = "AbilityGroupBox";
+            this.AbilityGroupBox.Size = new System.Drawing.Size(280, 340);
+            this.AbilityGroupBox.TabIndex = 0;
+            this.AbilityGroupBox.TabStop = false;
+            this.AbilityGroupBox.Text = "Character Abilities";
+            // 
+            // RollButton
+            // 
+            this.RollButton.Location = new System.Drawing.Point(116, 372);
+            this.RollButton.Name = "RollButton";
+            this.RollButton.Size = new System.Drawing.Size(82, 31);
+            this.RollButton.TabIndex = 2;
+            this.RollButton.Text = "Roll";
+            this.RollButton.UseVisualStyleBackColor = true;
+            this.RollButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PhycheLabel
+            // 
+            this.PhycheLabel.AutoSize = true;
+            this.PhycheLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PhycheLabel.Location = new System.Drawing.Point(61, 237);
+            this.PhycheLabel.Name = "PhycheLabel";
+            this.PhycheLabel.Size = new System.Drawing.Size(88, 24);
+            this.PhycheLabel.TabIndex = 1;
+            this.PhycheLabel.Text = "PHYCHE";
+            // 
+            // IntuitionLabel
+            // 
+            this.IntuitionLabel.AutoSize = true;
+            this.IntuitionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IntuitionLabel.Location = new System.Drawing.Point(46, 202);
+            this.IntuitionLabel.Name = "IntuitionLabel";
+            this.IntuitionLabel.Size = new System.Drawing.Size(102, 24);
+            this.IntuitionLabel.TabIndex = 1;
+            this.IntuitionLabel.Text = "INTUITION";
+            // 
+            // ReasonLabel
+            // 
+            this.ReasonLabel.AutoSize = true;
+            this.ReasonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReasonLabel.Location = new System.Drawing.Point(58, 167);
+            this.ReasonLabel.Name = "ReasonLabel";
+            this.ReasonLabel.Size = new System.Drawing.Size(90, 24);
+            this.ReasonLabel.TabIndex = 1;
+            this.ReasonLabel.Text = "REASON";
+            // 
+            // EnduranceLabel
+            // 
+            this.EnduranceLabel.AutoSize = true;
+            this.EnduranceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EnduranceLabel.Location = new System.Drawing.Point(19, 132);
+            this.EnduranceLabel.Name = "EnduranceLabel";
+            this.EnduranceLabel.Size = new System.Drawing.Size(129, 24);
+            this.EnduranceLabel.TabIndex = 1;
+            this.EnduranceLabel.Text = "ENDURANCE";
+            // 
+            // StrengthLabel
+            // 
+            this.StrengthLabel.AutoSize = true;
+            this.StrengthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StrengthLabel.Location = new System.Drawing.Point(34, 97);
+            this.StrengthLabel.Name = "StrengthLabel";
+            this.StrengthLabel.Size = new System.Drawing.Size(114, 24);
+            this.StrengthLabel.TabIndex = 1;
+            this.StrengthLabel.Text = "STRENGTH";
+            // 
+            // AgilityLabel
+            // 
+            this.AgilityLabel.AutoSize = true;
+            this.AgilityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AgilityLabel.Location = new System.Drawing.Point(69, 62);
+            this.AgilityLabel.Name = "AgilityLabel";
+            this.AgilityLabel.Size = new System.Drawing.Size(79, 24);
+            this.AgilityLabel.TabIndex = 1;
+            this.AgilityLabel.Text = "AGILITY";
+            // 
+            // FightingLabel
+            // 
+            this.FightingLabel.AutoSize = true;
+            this.FightingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FightingLabel.Location = new System.Drawing.Point(50, 27);
+            this.FightingLabel.Name = "FightingLabel";
+            this.FightingLabel.Size = new System.Drawing.Size(98, 24);
+            this.FightingLabel.TabIndex = 1;
+            this.FightingLabel.Text = "FIGHTING";
+            // 
+            // PhycheTextBox
+            // 
+            this.PhycheTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PhycheTextBox.Location = new System.Drawing.Point(163, 234);
+            this.PhycheTextBox.Name = "PhycheTextBox";
+            this.PhycheTextBox.Size = new System.Drawing.Size(100, 29);
+            this.PhycheTextBox.TabIndex = 0;
+            this.PhycheTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // IntuitionTextBox
+            // 
+            this.IntuitionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IntuitionTextBox.Location = new System.Drawing.Point(163, 199);
+            this.IntuitionTextBox.Name = "IntuitionTextBox";
+            this.IntuitionTextBox.Size = new System.Drawing.Size(100, 29);
+            this.IntuitionTextBox.TabIndex = 0;
+            this.IntuitionTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ReasonTextBox
+            // 
+            this.ReasonTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReasonTextBox.Location = new System.Drawing.Point(163, 164);
+            this.ReasonTextBox.Name = "ReasonTextBox";
+            this.ReasonTextBox.Size = new System.Drawing.Size(100, 29);
+            this.ReasonTextBox.TabIndex = 0;
+            this.ReasonTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // StrengthTextBox
+            // 
+            this.StrengthTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StrengthTextBox.Location = new System.Drawing.Point(163, 94);
+            this.StrengthTextBox.Name = "StrengthTextBox";
+            this.StrengthTextBox.Size = new System.Drawing.Size(100, 29);
+            this.StrengthTextBox.TabIndex = 0;
+            this.StrengthTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // EnduranceTextBox
+            // 
+            this.EnduranceTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EnduranceTextBox.Location = new System.Drawing.Point(163, 129);
+            this.EnduranceTextBox.Name = "EnduranceTextBox";
+            this.EnduranceTextBox.Size = new System.Drawing.Size(100, 29);
+            this.EnduranceTextBox.TabIndex = 0;
+            this.EnduranceTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // AgilityTextBox
+            // 
+            this.AgilityTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AgilityTextBox.Location = new System.Drawing.Point(163, 59);
+            this.AgilityTextBox.Name = "AgilityTextBox";
+            this.AgilityTextBox.Size = new System.Drawing.Size(100, 29);
+            this.AgilityTextBox.TabIndex = 0;
+            this.AgilityTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // FightingTextBox
+            // 
+            this.FightingTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FightingTextBox.Location = new System.Drawing.Point(163, 24);
+            this.FightingTextBox.Name = "FightingTextBox";
+            this.FightingTextBox.Size = new System.Drawing.Size(100, 29);
+            this.FightingTextBox.TabIndex = 0;
+            this.FightingTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // AbilityForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 442);
+            this.Controls.Add(this.RollButton);
+            this.Controls.Add(this.AbilityGroupBox);
+            this.Name = "AbilityForm1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ability Form";
+            this.AbilityGroupBox.ResumeLayout(false);
+            this.AbilityGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox AbilityGroupBox;
+        private System.Windows.Forms.Label FightingLabel;
+        private System.Windows.Forms.TextBox FightingTextBox;
+        private System.Windows.Forms.Label PhycheLabel;
+        private System.Windows.Forms.Label IntuitionLabel;
+        private System.Windows.Forms.Label ReasonLabel;
+        private System.Windows.Forms.Label EnduranceLabel;
+        private System.Windows.Forms.Label AgilityLabel;
+        private System.Windows.Forms.TextBox PhycheTextBox;
+        private System.Windows.Forms.TextBox IntuitionTextBox;
+        private System.Windows.Forms.TextBox ReasonTextBox;
+        private System.Windows.Forms.TextBox EnduranceTextBox;
+        private System.Windows.Forms.TextBox AgilityTextBox;
+        private System.Windows.Forms.Label StrengthLabel;
+        private System.Windows.Forms.TextBox StrengthTextBox;
+        private System.Windows.Forms.Button RollButton;
+    }
+}
+
---
+++ PracticalExam02/AbilityForm.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * File: AblityForm
+ * Author's Name: Able Varghese
+ * Date:03/25/2015
+ * App Description: App to randomly generating roll playing characters ability scores each time
+ * when the roll button is clicked.
+ 
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticalExam02
+{
+    //Ability form is a subclass inherited from the Form Class
+    public partial class AbilityForm1 : Form
+    {
+        public AbilityForm1()
+        {
+            InitializeComponent();
+        }
+
+        Random Rolling = new Random();
+
+    //button1_Click is a Method for the event, which is click of the roll button
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+    /*
+    Statement 41 to 47 below is generating a random numbers and then converting it into string 
+    and displaying it in the respective textboxes as output at the run time.
+        */
+
+            FightingTextBox.Text = Convert.ToString(Rolling.Next(1, 101));
+            AgilityTextBox.Text = Convert.ToString(Rolling.Next(1, 101));
+            StrengthTextBox.Text = Convert.ToString(Rolling.Next(1, 101));
+            EnduranceTextBox.Text = Convert.ToString(Rolling.Next(1, 101));
+            ReasonTextBox.Text = Convert.ToString(Rolling.Next(1, 101));
+            IntuitionTextBox.Text = Convert.ToString(Rolling.Next(1, 101));
+            PhycheTextBox.Text = Convert.ToString(Rolling.Next(1, 101));
+
+
+
+        }
+
+
+       
+    }
+}
